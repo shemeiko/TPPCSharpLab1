536e311beff43b7706616c18c129e04020863e86
Taras Shemeiko <shemiks2005@gmail.com>
2025-09-05 23:22:24 +0300

    compare issue fixed

--- ConsoleApp1/Task1.cs
+++ ConsoleApp1/Task1.cs
@@ -15,11 +15,11 @@ namespace Tasks {
             Console.Write("Enter number for C: ");
             double c = Convert.ToDouble(Console.ReadLine());
 
-            if ((a < b && b < c) || (b > a && b < c))
+            if ((a < b && b < c) || (a > b && b > c))
             {
                 Console.WriteLine(b);
             }
-            else if ((b > a && a < c) || (b < a && a > c)) 
+            else if ((b > a && a > c) || (b > a && a > c)) 
             {
                 Console.WriteLine(a);
             }
